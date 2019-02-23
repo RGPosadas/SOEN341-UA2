16b91671f76e4b4bfc5e3ceb9d9450b474fdf180
cindyslittleplanet <lo_cindy@outlook.com>
2019-02-23 10:41:25 -0500

    TASK-25: Issue #64, added username and timestamp

--- src/UserDatabase/database_models/tweet_model.js
+++ src/UserDatabase/database_models/tweet_model.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 const shortid = require("shortid");
+var timestamp = require('mongoose-timestamp');
 
 var userSchema = require('mongoose').model('User').schema;
 
@@ -9,12 +10,13 @@ var tweetSchema = mongoose.Schema({
   user_id: String,
   first_name: String,
   last_name: String
-  
-});
+}); 
+
+// Creating timestamp for tweetSchema
+tweetSchema.plugin(timestamp);
 
 //create the user model
 const Tweet = mongoose.model('Tweet', tweetSchema);
-
 
 //to use this User model, using module.export
 module.exports = Tweet;
