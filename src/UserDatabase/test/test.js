3ee842312fd7a99395d4665ad5ba2f826f13af58
dabris <dabris16.x@gmail.com>
2019-03-07 10:08:55 -0500

    TASK-30: Issue #74, Able to tweet commit# and commit message

--- src/UserDatabase/test/test.js
+++ src/UserDatabase/test/test.js
@@ -1,7 +1,4 @@
-console.log(process.env.TRAVIS_COMMIT);
-console.log(process.env.TRAVIS_COMMIT_MESSAGE);
 var User, app, mongoose, request, server, should, user, agent;
-
 should   = require("should");
 app      = require("../server");
 mongoose = require("mongoose");
@@ -9,14 +6,21 @@ const Tweet = require("../database_models/tweet_model");
 request  = require("supertest");
 agent = request.agent(app);
 
+
+
+
 const userCredentials = {
     email: 'travis@travis.com',
     password: 'travis'
 }
+
 const wrongCredentials ={
   email:'travis@travis.com',
   password: 'wrongpassword'
 }
+
+
+
 describe('Login Test', function () {
   describe('Login test begins', function () {
       it('The app should redirect to /profile when logged in successfully', function (done) {
@@ -40,55 +44,56 @@ describe('Login Test', function () {
       }
       )
 
-  after(function(done) {
+  afterEach(function(done) {
+      return done();
+  });
+
+  after(function () {
       console.log("    Login test ends");
-    // process.exit();
-    });
+  });
 
 })
 })
 
 
 
-describe('Tweet Test', function () {
-    var travisCommitMessage = '<%= process.env.TRAVIS_COMMIT_MESSAGE %>';
-    before(function(done) {
+describe('Tweet Test:sending a tweet...', function () {
+    var tweetToPost = "[Travis-CI Testing] Commit " + String(process.env.TRAVIS_COMMIT) + "/" + String(process.env.TRAVIS_COMMIT_MESSAGE);
+    
+    before(function (done) {
+       
       tweet = new Tweet({
-          tweet: travisCommitMessage,
+          tweet:tweetToPost,
       user_id: "travis",
       first_name: "travis",
       last_name: "travis",
       liked_by: []
       });
-      tweet.save(done)
-    });
+      tweet.save(done)});
+  
     
-  describe('A tweet has been posted. Tweet test begins', function () {
-      it('The app should redirect to /profile when logged in successfully', function (done) {
-        agent
-        .post('/users/login')
-         .send(userCredentials)
-        .expect('Location','/profile')
-        .end(done)
+  describe('tweet sended to db:'+tweetToPost, function () {
 
-      }
-      )
-      it('The app should redirect to /login when login failed', function (done) {
-      {
-        agent
-        .post('/users/login')
-         .send(wrongCredentials)
-        .expect('Location','/users/login')
-        .end(done)
-      } 
-        
-      }
-      )
+      it('A tweet has been posted', function (done) {
+       done();
+      })
 
-  after(function(done) {
-      console.log("    Tweet test ends");
-     process.exit();
-    });
+      
+      after(function(){
+          var tweetToPost = "[Travis-CI Testing] Commit " + String(process.env.TRAVIS_COMMIT) + "/" + String(process.env.TRAVIS_COMMIT_MESSAGE);      
+          Tweet.findOne({ tweet: tweetToPost }, function (err, data) {
+          if (err) throw err;
+          console.log("Query results: " + data);
+            if(data.tweet == tweetToPost){
+                console.log("Tweet successfully found in database");
+                process.exit();
+            }
+           
+      });
+   
+      })
 
 })
 })
+
+ 
