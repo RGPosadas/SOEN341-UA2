78160e25f1a2f51aa45fe36a411ad8b982ca5c0a
dabris <dabris16.x@gmail.com>
2019-03-07 02:00:50 -0500

    TASK-30: Issue #74, able to post

--- src/UserDatabase/test/test.js
+++ src/UserDatabase/test/test.js
@@ -1,9 +1,11 @@
+console.log(process.env.TRAVIS_COMMIT);
+console.log(process.env.TRAVIS_COMMIT_MESSAGE);
 var User, app, mongoose, request, server, should, user, agent;
 
 should   = require("should");
 app      = require("../server");
 mongoose = require("mongoose");
-User     = mongoose.model("User");
+const Tweet = require("../database_models/tweet_model");
 request  = require("supertest");
 agent = request.agent(app);
 
@@ -23,10 +25,7 @@ describe('Login Test', function () {
          .send(userCredentials)
         .expect('Location','/profile')
         .end(done)
-//         .end(function (err, res) {
-//                console.log('***************************Authenticated*********************************************');
-//                done();
-//            });
+
       }
       )
       it('The app should redirect to /login when login failed', function (done) {
@@ -43,6 +42,51 @@ describe('Login Test', function () {
 
   after(function(done) {
       console.log("    Login test ends");
+    // process.exit();
+    });
+
+})
+})
+
+
+
+describe('Tweet Test', function () {
+    var travisCommitMessage = '<%= process.env.TRAVIS_COMMIT_MESSAGE %>';
+    before(function(done) {
+      tweet = new Tweet({
+          tweet: travisCommitMessage,
+      user_id: "travis",
+      first_name: "travis",
+      last_name: "travis",
+      liked_by: []
+      });
+      tweet.save(done)
+    });
+    
+  describe('A tweet has been posted. Tweet test begins', function () {
+      it('The app should redirect to /profile when logged in successfully', function (done) {
+        agent
+        .post('/users/login')
+         .send(userCredentials)
+        .expect('Location','/profile')
+        .end(done)
+
+      }
+      )
+      it('The app should redirect to /login when login failed', function (done) {
+      {
+        agent
+        .post('/users/login')
+         .send(wrongCredentials)
+        .expect('Location','/users/login')
+        .end(done)
+      } 
+        
+      }
+      )
+
+  after(function(done) {
+      console.log("    Tweet test ends");
      process.exit();
     });
 
