adb4d69024252b7f804b8e9cabcb413a7eb3abd7
kajanthyS <35846357+kajanthyS@users.noreply.github.com>
2019-03-19 22:52:21 -0400

    TASK-31, Issue #75, UNFOLLOW unit test added

--- src/UserDatabase/test/test.js
+++ src/UserDatabase/test/test.js
@@ -9,34 +9,6 @@ agent = request.agent(app);
 const shortid = require("shortid");
 var ObjectId = mongoose.Types.ObjectId
 
-/* user1 = new User({
-  first_name: "user1",
-last_name: "user1",
-email: "user1@kiwi.com",
-password: "test",
-//member_id: shortid.generate(),
-follower:[],
-following: [],
-location: "test",
-description: "test",
-interests: "test",
-}); 
-console.log(user1);
-
-user2 = new User({   
-  first_name: "user2",
-last_name: "user2",
-email: "user2@kiwi.com",
-password: "test",
-//member_id: shortid.generate(),
-follower:[],
-following: [],
-location: "test",
-description: "test",
-interests: "test",
-});
-*/
-
 const userCredentials = {
     email: 'travis@travis.com',
     password: 'travis'
@@ -152,11 +124,10 @@ describe('Tweet Test: following another user...', function(){
     last_name: "user1",
     email: "user1@kiwi.com",
     password: "test",
-    //member_id: shortid.generate(),
     follower:[],
     following: [],
-    location: "ayylmao",
-    description: "ayylmao",
+    location: "location2",
+    description: "location2",
     interests: "test",
     }); 
 
@@ -165,15 +136,14 @@ describe('Tweet Test: following another user...', function(){
     last_name: "user2",
     email: "user2@kiwi.com",
     password: "test",
-    //member_id: shortid.generate(),
     follower:[],
     following: [],
     location: "test",
     description: "test",
     interests: "test",
     });
-    //User.updateOne({"_id": ObjectId(user1.id)}, { $push: { "following": ObjectId(user2.id)} });//user1 is following user2
-    user1.following.push(ObjectId(user2.id)); // wtf mongo are u kidding me ? 
+    //user1 is following user2
+    user1.following.push(ObjectId(user2.id)); 
 user2.save();
 user1.save(done);
 
@@ -187,43 +157,67 @@ describe('following was sent to db: ' , function(){
   after(function(){
 
     User.findOne({"_id": ObjectId(user1.id)}, function (err, data){//searching for user2 in the following array
-      //var exists =data.following.some(o => o.id == user2.id);
       if (err) throw err;
-      console.log(data.following.includes(user2.id));// these console logs are just to see the result, pls clean it after
-      console.log(data.following);
-      console.log(user2.id);
-      console.log("Query results: "+ data); 
-      if(data.following.indexOf(user2.id)==0){  // somehow includes() doesnt work so i have to use indexOf lmao 
-        //if(exists){
+      //console.log(data.following.includes(user2.id));
+      //console.log(data.following);
+      //console.log(user2.id);
+     // console.log("Query results: "+ data); 
+      if(data.following.indexOf(user2.id)==0){  
         console.log("Following user2 successfully!");
-        process.exit();
+        //process.exit();
       }
     });
   })
 })
 })
-/*
-//testing ability to FOLLOW 
-describe('Tweet Test: Travis being followed by another user...', function(){
-  before (function(done){
-    User.updateOne({_id:user2.id}, {$push: {"follower": user1.id}});
-    user2.save(done);
+
+//testing if Travis is UNFOLLOWING properly 
+describe('Tweet Test: unfollowing another user ...', function(){
+
+  before(function(done){
+    user1 = new User({
+      first_name: "user1",
+    last_name: "user1",
+    email: "user1@kiwi.com",
+    password: "test",
+    follower:[],
+    following: [],
+    location: "location2",
+    description: "location2",
+    interests: "test",
+    }); 
+
+    user2 = new User({   
+      first_name: "user2",
+    last_name: "user2",
+    email: "user2@kiwi.com",
+    password: "test",
+    follower:[],
+    following: [],
+    location: "test",
+    description: "test",
+    interests: "test",
+    });
+
+    user1.following.push(ObjectId(user2.id)); 
+    user2.save();
+    console.log(user1);
+    user1.following.shift();
+    user1.save(done);
+    console.log(user1);
   });
-  describe('user2 was sent to db: ', function(){
-    it('Travis is now being followed by the user ', function(done){
+  describe('unfollow was sent to the db: ', function(){
+    it('Travis has unfollowed the user ', function(done){
       done();
     })
-    after(function(){
-      User.findOne({first_name:"user1"}, function(err,data){
-        if(err) throw err;
-        console.log("Query results: "+ data);
-        if(data.user2.includes(user1.id)){//this throws an error//there is soemthing wrong with the includes and the way the id id is being fetched
-          console.log("Being followed successfully! ");
-          process.exit();
-        }
-      });
-    })
+   after(function(){
+    User.findOne({"_id": ObjectId(user1.id)}, function (err, data){
+      if (err) throw err;
+      if (data.following.indexOf(user2.id==null)){
+        console.log("Unfollowing successfully!");
+      process.exit();
+      }
+    });
+   })
   })
 })
-*/
-
