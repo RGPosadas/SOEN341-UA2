8f449d68a0c7d9c0f37cd065bd506f6d56a369ad
Tung Leu <leudeptrai@gmail.com>
2019-03-21 19:42:29 -0400

    BUG-1: Issue #87, tweets can be seen in the feed

--- src/UserDatabase/test/test.js
+++ src/UserDatabase/test/test.js
@@ -87,7 +87,7 @@ describe('Tweet Test:sending a tweet...', function () {
         first_name: "Tweet",
       last_name: "Poster",
       email: "tweetposter@kiwi.com",
-      password: "test",
+      password: "Jeneparlepasfrancais123",
       follower:[],
       following: [],
       location: "test",
@@ -102,6 +102,7 @@ describe('Tweet Test:sending a tweet...', function () {
       last_name: tweetPoster.last_name,
       liked_by: []
       });
+      tweetPoster.save();
       tweet.save(done)});
 
 
@@ -142,6 +143,7 @@ describe('Tweet Test:liking a tweet...', function () {
     }); 
     //Tweet.update({user_id: tweetPoster.id},{$push:{liked_by : tweetLiker.id}});
     tweet.liked_by.push(tweetLiker.id);
+    tweetLiker.save();
     tweet.save(done)
   });
 
