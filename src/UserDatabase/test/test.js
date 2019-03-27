04eae24986433f1dd494e4fc5b3d928c1f1f0753
kajanthyS <35846357+kajanthyS@users.noreply.github.com>
2019-03-26 21:13:08 -0400

    TASK-31, Issue #75, Added UNFOLLOW

--- src/UserDatabase/test/test.js
+++ src/UserDatabase/test/test.js
@@ -26,7 +26,29 @@ const wrongEmail ={
     password: 'wrongpassword'
 }
 
+      user1 = new User({
+        first_name: "Tweet",
+      last_name: "Poster",
+      email: "follow@kiwi.com",
+      password: "CornFlakes",
+      follower:[],
+      following: [],
+      location: "IGA",
+      description: "Flaky",
+      interests: "Grains",
+      }); 
 
+      user2 = new User({
+        first_name: "Life",
+      last_name: "Cereal",
+      email: "follower@kiwi.com",
+      password: "LifeCereal",
+      follower:[],
+      following: [],
+      location: "IGA",
+      description: "Full of Life",
+      interests: "Corn Flakes",
+      }); 
 
 describe('Login Test', function () {
   describe('Login test begins', function () {
@@ -84,26 +106,15 @@ describe('Tweet Test:sending a tweet...', function () {
     var tweetToPost = "[Travis-CI Testing] Commit " + String(process.env.TRAVIS_COMMIT) + "/" + String(process.env.TRAVIS_COMMIT_MESSAGE);
 
     before(function (done) {
-      tweetPoster = new User({
-        first_name: "Tweet",
-      last_name: "Poster",
-      email: "tweetposter@kiwi.com",
-      password: "Jeneparlepasfrancais123",
-      follower:[],
-      following: [],
-      location: "test",
-      description: "test",
-      interests: "test",
-      }); 
-
+        
       tweet = new Tweet({
           tweet:tweetToPost,
-      user_id: tweetPoster.id,
-      first_name: tweetPoster.first_name,
-      last_name: tweetPoster.last_name,
+      user_id: user1.id,
+      first_name: user1.first_name,
+      last_name: user1.last_name,
       liked_by: []
       });
-      tweetPoster.save();
+      user1.save();
       tweet.save(done)});
 
 
@@ -116,7 +127,7 @@ describe('Tweet Test:sending a tweet...', function () {
 
       after(function(){
           var tweetToPost = "[Travis-CI Testing] Commit " + String(process.env.TRAVIS_COMMIT) + "/" + String(process.env.TRAVIS_COMMIT_MESSAGE);
-          Tweet.findOne({ user_id: tweetPoster.id }, function (err, data) {
+          Tweet.findOne({ user_id: user1.id }, function (err, data) {
           if (err) throw err;
           console.log("Query results: " + data);
             if(data.tweet == tweetToPost){
@@ -130,24 +141,7 @@ describe('Tweet Test:sending a tweet...', function () {
 describe('Tweet Test:liking a tweet...', function () {
   var tweetToPost = "[Travis-CI Testing] Commit " + String(process.env.TRAVIS_COMMIT) + "/" + String(process.env.TRAVIS_COMMIT_MESSAGE);
   
-  before(function (done) {
-    tweetLiker = new User({
-      first_name: "Tweet",
-    last_name: "Liker",
-    email: "tweetliker@kiwi.com",
-    password: "test",
-    follower:[],
-    following: [],
-    location: "test",
-    description: "test",
-    interests: "test",
-    }); 
-    tweet.liked_by.push(tweetLiker.id);
-    tweetLiker.save();
-    tweet.save(done)
-  });
-
-  
+  before(function (done) {  
 describe('like sended to db:'+tweetToPost, function () {
 
     it('The tweet was liked by travis', function (done) {
@@ -157,10 +151,10 @@ describe('like sended to db:'+tweetToPost, function () {
     
     after(function(){
         var tweetToPost = "[Travis-CI Testing] Commit " + String(process.env.TRAVIS_COMMIT) + "/" + String(process.env.TRAVIS_COMMIT_MESSAGE);      
-        Tweet.findOne({ user_id: tweetPoster.id }, function (err, data) {
+        Tweet.findOne({ user_id: user1.id }, function (err, data) {
         if (err) throw err;
         console.log("Query results: " + data);
-          if(data.liked_by.indexOf(tweetLiker.id)==0){
+          if(data.liked_by.indexOf(user1.id)==0){
               console.log("Liked successfully");
           }
          
@@ -174,29 +168,7 @@ describe('like sended to db:'+tweetToPost, function () {
 describe('Tweet Test: following another user...', function(){
 
   before(function(done){ 
-    user1 = new User({
-      first_name: "user1",
-    last_name: "user1",
-    email: "user1@kiwi.com",
-    password: "test",
-    follower:[],
-    following: [],
-    location: "location2",
-    description: "location2",
-    interests: "test",
-    }); 
-
-    user2 = new User({   
-      first_name: "user2",
-    last_name: "user2",
-    email: "user2@kiwi.com",
-    password: "test",
-    follower:[],
-    following: [],
-    location: "test",
-    description: "test",
-    interests: "test",
-    });
+      
     user1.following.push(ObjectId(user2.id)); 
     user2.save();
     user1.save(done);
@@ -220,4 +192,30 @@ describe('following was sent to db: ' , function(){
   })
 })
 })
- 
+
+describe('Tweet Test: unfollowing another user ...', function(){
+
+  before(function(done){
+    user1.following.push(ObjectId(user2.id)); 
+    user2.save();
+    console.log(user1);
+    user1.following.shift();
+    user1.save(done);
+    console.log(user1);
+  });
+  describe('unfollow was sent to the db: ', function(){
+    it('Travis has unfollowed the user ', function(done){
+      done();
+    })
+   after(function(){
+    User.findOne({"_id": ObjectId(user1.id)}, function (err, data){
+      if (err) throw err;
+      if (data.following.indexOf(user2.id==null)){
+        console.log("Unfollowing successfully!");
+      process.exit();
+      }
+    });
+   })
+  })
+})
+
