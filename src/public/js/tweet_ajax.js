59293a2343e83095a71ab395d491f79b25b2b2c6
kajanthyS <35846357+kajanthyS@users.noreply.github.com>
2019-03-29 10:45:33 -0400

    TASK-38: Issue #90, Deleted commented out code

--- src/public/js/tweet_ajax.js
+++ src/public/js/tweet_ajax.js
@@ -1,31 +1,3 @@
-// // when document is ready, run this function
-// $(document).ready(function(){
-//   // when you have a form and in that form you have an event of submit fire this function
-//   $('form').on('submit', function(){
-//       console.log("Hello");
-//       var item = $('form input'); // locate the input in the form
-//       var todo = {item: item.val()}; // grab the value from the input
-//       console.log(item.val());
-//       $.ajax({
-//         type: 'POST',
-//         url: '/profile',
-//         data: todo, // passing this data to app.post
-//         //manipulate it in the app.post body
-//         //send the updated data to the success: at the bottom
-//         //Do something with that updated data
-//         success: function(data){//new data
-//           //do something with the data via front-end framework
-//           location.reload();
-//         }
-//       });
-
-//       return false;
-
-//   });
-
- 
-// });
-
 $(document).ready(function(){
   console.log("document ready")
   $('form').on('submit', function(){
@@ -42,10 +14,6 @@ $(document).ready(function(){
           location.reload();
         }
       });
-
       return false;
-
   });
-
-
 });
