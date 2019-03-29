9cb8492f3c7ffe2267f779fb194332ca003f2c77
Nhut Vo <nhutmvo1997@gmail.com>
2019-03-29 10:48:24 -0400

    TASK-38: Issue #90, Deleted commented out code

--- src/public/js/unfollow.js
+++ src/public/js/unfollow.js
@@ -11,7 +11,6 @@ $(document).ready(function(){
 
     $('form').on('submit', function(){
 
-        //alert("You are now unfollowing!");
         $.ajax({
           type: 'POST',
           url: '/unfollow',
