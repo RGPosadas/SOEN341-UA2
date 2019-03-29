b465a4a8f3ed23b1b5944d91f19c36417acc36b2
kajanthyS <35846357+kajanthyS@users.noreply.github.com>
2019-03-29 10:34:54 -0400

    TASK-38, Issue #90, Deleted unused code

--- src/database_models/user_model.js
+++ src/database_models/user_model.js
@@ -1,9 +1,6 @@
 const mongoose = require("mongoose");
 const shortid = require("shortid");
 
-//Mongoose is a JavaScript library that allows you to define schemas with strongly typed data.
-// For more information, please visit this link: "https://code.tutsplus.com/articles/an-introduction-to-mongoose-for-mongodb-and-nodejs--cms-29527"
-
 // Creating a userSchema
 var userSchema = mongoose.Schema({
   first_name: String,
@@ -16,29 +13,10 @@ var userSchema = mongoose.Schema({
   location: String,
   description: String,
   interests: String,
-  // tweet: [
-  //   {
-  //     type: mongoose.Schema.Type.ObjectId,
-  //     ref: "tweet_model"
-  //   }
-  // ]
-  // like: [
-  //   {
-  //     type: mongoose.Schema.Type.ObjectId,
-  //     ref: "tweet_model"
-  //   }
-  // ]
-  //profile_pic: {type: String, default: "default_profile.png"}
 });
-
 
 //create the user model
 const User = mongoose.model('User', userSchema);
-
-// Testing value to see if it appears on Collections of DB
-//   User.create({
-//     first_name: "John",
-//   });
 
 //to use this User model, using module.export
 module.exports = User; 
